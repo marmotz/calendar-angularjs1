a0f4b8250d7d11459d6b3edd94128999f7637f82
Renaud LITTOLFF <rlittolff@gmail.com>
2016-10-17 07:50:01 +0200

    Application initialization

---
+++ app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular
+    .module('myApp', [ 'ngRoute' ])
+    .config(
+        [
+            '$locationProvider', '$routeProvider',
+            function($locationProvider, $routeProvider) {
+                $locationProvider.hashPrefix('!');
+            }
+        ]
+    )
+;
